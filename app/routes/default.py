77c6e06d1363fe5508e8050f631c8d85252ef910
Timur <timeosji09@gmail.com>
2025-03-14 19:41:28 +0200

    commit

--- app/routes/default.py
+++ app/routes/default.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
+from sqlalchemy.orm import Session
+from sqlalchemy import desc
+from app.db.models.base import engine
+from app.db.models.data import GameBarDB
 import requests
 
+session = Session(engine)
 @app.route("/")
-def hello_world():
-    return "Welcome!"
+def main_page():
+    top = session.query(GameBarDB).order_by(desc(GameBarDB.rating)).limit(6).all()
+    return render_template("index.html", top=top)
