baa420cc777452b483ee65c0036aa91d32ad8b53
Timur <timeosji09@gmail.com>
2025-03-14 19:51:12 +0200

    commit

--- app/routes/default.py
+++ app/routes/default.py
@@ -1,13 +1,15 @@
 from flask import render_template
 from app import app
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
+from sqlalchemy import desc, func
 from app.db.models.base import engine
 from app.db.models.data import GameBarDB
+
 import requests
 
 session = Session(engine)
 @app.route("/")
 def main_page():
+    random_games = session.query(GameBarDB).order_by(func.random()).limit(6).all()
     top = session.query(GameBarDB).order_by(desc(GameBarDB.rating)).limit(6).all()
-    return render_template("index.html", top=top)
+    return render_template("index.html", top=top, random_games=random_games)
