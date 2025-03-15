147b7b6db7d17f8c8f09b1ed8af5cc7548c30401
Timur <timeosji09@gmail.com>
2025-03-15 16:31:52 +0200

    commit

--- app/routes/default.py
+++ app/routes/default.py
@@ -8,8 +8,24 @@ from app.db.models.data import GameBarDB
 import requests
 
 session = Session(engine)
-@app.route("/")
+@app.get("/")
 def main_page():
     random_games = session.query(GameBarDB).order_by(func.random()).limit(6).all()
     top = session.query(GameBarDB).order_by(desc(GameBarDB.rating)).limit(6).all()
     return render_template("index.html", top=top, random_games=random_games)
+
+@app.get("/catalog/")
+def games_list():
+    games = session.query(GameBarDB)
+    return render_template("catalog.html", games=games)
+
+@app.get("/game/<id>/")
+def game_page(id):
+    game = session.query(GameBarDB).filter(GameBarDB.id == id).first()
+    return render_template("game.html",
+                           name=game.name,
+                           description=game.description,
+                           genre=game.genre,
+                           rating=game.rating,
+                           picture=game.picture
+                           )
